1c354b7a6cae6394e836f5a16e62340426893917
Cydnirn <seno@siotics.org>
2024-01-15 18:12:29 +0700

    fix: fix some issues

--- script_seno.sh
+++ script_seno.sh
@@ -2,9 +2,8 @@
 
 # This function get memory, convert the memory to MB from GB and pause for 3 seconds.
 get_memory(){
-    memory=$(awk 'memory$3=="kB"{$2=$2/1024;$3="MB"} 1' /proc/meminfo | grep "MemTotal" | awk '{print $2 " " $3}')
+    memory=$(free --mega | grep "Mem" | awk '{print $2 "MB"}')
     echo "Fetching total memory..."
-    sleep 1s
     echo "Total memory: $memory"
     echo -e "\n"
     sleep 3s
@@ -13,17 +12,15 @@ get_memory(){
 # This function get disk space, convert the disk space to MB from GB and pause for 3 seconds.
 get_disk_space(){
     echo "Fetching disk space..."
-    sleep 1s
     df -l -BG --output=source,size
     echo -e "\n"
     sleep 3s
 }
 
-# This function get disk space with format of ext4 only and display the source and used percent
+# This function get disk space with format of ext4 only and display the source and used percent and omit tmpfs
 get_ext4(){
     echo "Fetching disk space with format of ext4 only..."
-    sleep 1s
-    df -lh -t ext4 --output=source,pcent
+    df -lh -t ext4 -x tmpfs --output=source,pcent
     echo -e "\n"
     sleep 1m
 }
